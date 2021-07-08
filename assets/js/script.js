4cc877ca941c16d5df9c60b31fa803e521de9b76
Nathan Batchelder <natebatchelder@gmail.com>
2021-07-07 20:38:19 -0700

    added variables to SCRIPT.JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,18 +10,44 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
+// creates the password given the selected variables
 function generatePassword() {
-	let yourNewPassword = '';
+	var finalPass = "";
+	var howLong = prompt("Between 8 and 128, how long will your password be?", "(Pick a number between 8 and 128.)");
+	
+if(howLong <8 || howLong >128) {
+	alert("Please pick a number between 8 and 128.")
+	generatePassword();
+}
+else {
+
+	var lowerCase =
+	var upperCase =
+	var numbers =
+	var specialCharacters =
+
+	var uppers 
+	var lowers 
+	var numbers 
+	var specials 
+	var selected = "";
+}
+	var yourNewPassword = '';
+
+	// rewrite this function without CharCode
 	
 	var lowerCase = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 	var	upperCase = String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 	var number = String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-	var symbol = symbols[Math.floor(Math.random() * symbols.length)];
+	var symbol = special[Math.floor(Math.random() * special.length)];
 	// create a loop
 	
 
 	return yourNewPassword;
 }
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
